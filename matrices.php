c71710e81fc01519bc91097eed960873a9adabea
Danieljur-1 <danieljrdmtnz@gmail.com>
2024-09-27 15:54:31 +0200

    matrices

--- matrices.php
+++ matrices.php
@@ -35,26 +35,44 @@
         
         $edades = ["Andrés" => 20, "Bárbara" => 19, "Camilo" => 17];
 
-        print "<p>Bárbara tiene  <span class='barbara'>$edades[Bárbara]</span> años.</p>\n";  //mira que Bárbara no está entre comillas
+        print "<p>Bárbara tiene <span class='barbara'>{$edades['Bárbara']}</span> años.</p>\n";  // Cambiado para usar comillas simples
         
-
-
         /* Array multidimensional asociativo */
         $datos = [
-
-          ["nombre" => "pepe", "edad" => 25, "pedo" => 80],
-          ["nombre" => "juan", "edad" => 22, "pedo" => 75],
+            ["nombre" => "pepe", "edad" => 25, "peso" => 80],
+            ["nombre" => "juan", "edad" => 22, "peso" => 75],
         ];
-
-        print "<p>{$datos[1]["nombre"]} pesa
-        {$datos[1]["peso"]} kilos
-        </p>\n";
-
-        print "\n";
         
-        print "<p>" . $datos[0]["nombre"] . " tiene " .
-            $datos[0]["edad"] . " años
-            </p>\n";
+        print "<p>{$datos[1]['nombre']} pesa {$datos[1]['peso']} kilos</p>\n"; // Cambiado para usar comillas simples
+        print "\n";
+        print "<p>" . $datos[0]["nombre"] . " tiene " . $datos[0]["edad"] . " años</p>\n";
+        
+        $datos = [];
+        $datos["nombre"] = "Santiago";
+        $datos["apellidos"] = "Ramón y Cajal";
+        
+        print "<pre>";
+        print_r($datos);
+        print "</pre>";
+        
+
+
+
+
+
+
+
+
+            $datos = [];
+            $datos[1]["nombre"] = "Santiago";
+            $datos[1]["apellidos"] = "Ramón y Cajal";
+            $datos[2]["nombre"] = "Leonardo";
+            $datos[2]["apellidos"] = "Torres Quevedo";
+            
+            print "<pre>";
+            print_r($datos);
+            print "</pre>";
+
 
     ?>
     
